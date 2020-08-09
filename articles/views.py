5ec0968fa66993097dea5e873735aa094936b15d
Lev <levgeneral@yandex.ru>
2020-08-09 18:37:07 +0300

    Added articles list and static files.

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from articles.models import Post
 
-# Create your views here.
+
+def articles_catalog(request):
+    context = {}
+
+    context['articles'] = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'articles-catalog.html', context)
