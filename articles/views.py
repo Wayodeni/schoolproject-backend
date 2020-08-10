a3f8891370f16c60c68ecee41ee16b1c89e3090e
Lev <levgeneral@yandex.ru>
2020-08-10 10:16:40 +0300

    Added article name -> article text url.

--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+
 from articles.models import Post
 
 
 def articles_catalog(request):
     context = {}
-
     context['articles'] = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'articles-catalog.html', context)
+
+
+def article(request, pk):
+    # post = get_object_or_404(Post, pk=pk)
+    post = Post.objects.get(pk=pk)
+    return render(request, 'article.html', {'post': post})
